955b553b403d4a19bb527054aeb7d32a52e6ed0e
Andrew Kallai <andrewka@udel.edu>
2023-03-04 13:08:05 -0500

    stringsToIntegers passes all tests

--- src/arrays.ts
+++ src/arrays.ts
@@ -24,7 +24,9 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const integers = numbers.map(
+        (str: string): number=> (isNaN(Number(str))) ? 0: Number(str));
+    return integers;
 }
 
 /**
