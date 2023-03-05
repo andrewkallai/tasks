f30ecda3e0584adbbb6fc6426f411d1432873ca8
Andrew Kallai <andrewka@udel.edu>
2023-03-05 18:29:26 -0500

    function toCSV passes all tests

--- src/nested.ts
+++ src/nested.ts
@@ -101,7 +101,19 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const output: string[] = questions.map(
+        (question: Question): string =>
+            question.id +
+            "," +
+            question.name +
+            "," +
+            question.options.length +
+            "," +
+            question.points +
+            "," +
+            question.published
+    );
+    return "id,name,options,points,published\n" + output.join("\n");
 }
 
 /**
