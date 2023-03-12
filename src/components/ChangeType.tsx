2695ae8b6625756abcad00709b7aa3969a8ff56a
Andrew Kallai <andrewka@udel.edu>
2023-03-12 19:32:35 -0400

    Modified ChangeType.tsx to pass tests and display button

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [question_type, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    const changeType = (): void => {
+        if (question_type === "short_answer_question") {
+            setQuestionType("multiple_choice_question");
+        } else {
+            setQuestionType("short_answer_question");
+        }
+    };
+    return (
+        <div>
+            <Button onClick={changeType}>Change Type</Button>
+            {question_type === "short_answer_question" ? (
+                <div>Short Answer</div>
+            ) : (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
 }
