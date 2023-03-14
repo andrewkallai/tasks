3ed2021bb44009d7b6b57610a8335c91a9773f6f
Andrew Kallai <andrewka@udel.edu>
2023-03-13 23:18:02 -0400

    StartAttempt function passes tests and shows up on website

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,46 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [numAttempts, setAttempts] = useState<number>(4);
+    const [progress, setProgress] = useState<boolean>(false);
+    const changeAttempts = (integer: number): void => {
+        setAttempts(numAttempts + integer);
+    };
+    const changeProgress = (value: boolean): void => {
+        setProgress(value);
+    };
+    return (
+        <div>
+            <div>
+                <Button
+                    onClick={(): void => {
+                        changeAttempts(-1);
+                        changeProgress(true);
+                    }}
+                    disabled={progress || !numAttempts}
+                >
+                    Start Quiz
+                </Button>
+                <Button
+                    onClick={(): void => {
+                        changeProgress(false);
+                    }}
+                    disabled={!progress}
+                >
+                    Stop Quiz
+                </Button>
+            </div>
+            <div>
+                <Button
+                    onClick={(): void => {
+                        changeAttempts(1);
+                    }}
+                    disabled={progress}
+                >
+                    Mulligan
+                </Button>
+                {numAttempts}
+            </div>
+        </div>
+    );
 }
