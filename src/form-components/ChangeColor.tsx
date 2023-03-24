22d361f0dbd3d3009dd1ba81fc9f60497b6b4faa
Andrew Kallai <andrewka@udel.edu>
2023-03-24 15:33:47 -0400

    function ChangeColor passes tests and displays

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
-
+import Form from "react-bootstrap/Form";
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>("red");
+    const changeColor = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setColor(event.target.value);
+    };
+    const colors: string[] = [
+        "red",
+        "yellow",
+        "green",
+        "blue",
+        "orange",
+        "purple",
+        "pink",
+        "brown"
+    ];
+    const components = colors.map((color_value: string) => {
+        return (
+            <Form.Check
+                inline
+                type="radio"
+                onChange={changeColor}
+                value={color_value}
+                checked={color === color_value}
+                key=""
+                label={
+                    <span style={{ backgroundColor: color_value }}>
+                        {color_value}
+                    </span>
+                }
+            />
+        );
+    });
     return (
         <div>
             <h3>Change Color</h3>
+            {components}
+            <div>
+                You have chosen:{" "}
+                <span
+                    data-testid="colored-box"
+                    style={{ backgroundColor: color }}
+                >
+                    {color}
+                </span>
+            </div>
         </div>
     );
 }
